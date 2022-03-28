55e59e94a79d282ea2a50ce910fb916dd201506e
Denilso Fabiano Brustelo <denilso.brustelo@gmail.com>
2022-03-28 14:25:24 -0300

    Create client resource, exception, properties, import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ana Martins',	'493.908.190-75', 15000.0, 	TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z',	1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Letícia Moraes',	'579.797.820-20',	18001.0,	TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z',	2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Arthur Moraes',	'963.250.410-03',	12002.0, 	TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z',	4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Heloísa Moura',	'968.738.220-19',	25003.0, 	TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z',	3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Thales Silveira',	'123.653.090-03',	35004.0,	TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z',	2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lívia Pinto',	'124.770.190-58',	17005.0,	TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z',	1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Isabel Barbosa',	'565.142.890-96',	5006.0,	TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z',	3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Noah Barbosa',	'226.728.390-50',	7007.0,	TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z',	2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Sr. Felipe Cunha',	'413.646.730-37',	9008.0,	TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z',	1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Cauê Teixeira', '066.831.140-13',	8009.0,	TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z',	2);
